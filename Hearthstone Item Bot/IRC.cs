698209620318eba5c6914368b2d542842e95d58b
Ben Buzbee <benbuzbee@gmail.com>
2013-11-17 16:16:38 -0800

    Basic spellcheck

--- Hearthstone Item Bot/IRC.cs
+++ Hearthstone Item Bot/IRC.cs
@@ -108,11 +108,64 @@ namespace HSBot
                 return match;
             
             // Otherwise search using contains
-
+            Cards.Card closestMatch = null;
+            int closestDistance = cardname.Length;
             foreach (Cards.Card c in cards.Values)
+            {
                 if (c.Name.ToLower().Contains(cardname.ToLower())) return c;
-            return null;
+                int distance = LevenshteinDistance(cardname.ToLower(), c.Name.ToLower());
+                if (distance <= 3 && distance < closestDistance)
+                {
+                    closestDistance = distance;
+                    closestMatch = c;
+                }
+            }
+            return closestMatch;
 
+        }
+        public static int LevenshteinDistance(string s, string t)
+        {
+            int n = s.Length;
+            int m = t.Length;
+            int[,] d = new int[n + 1, m + 1];
+
+            // Step 1
+            if (n == 0)
+            {
+                return m;
+            }
+
+            if (m == 0)
+            {
+                return n;
+            }
+
+            // Step 2
+            for (int i = 0; i <= n; d[i, 0] = i++)
+            {
+            }
+
+            for (int j = 0; j <= m; d[0, j] = j++)
+            {
+            }
+
+            // Step 3
+            for (int i = 1; i <= n; i++)
+            {
+                //Step 4
+                for (int j = 1; j <= m; j++)
+                {
+                    // Step 5
+                    int cost = (t[j - 1] == s[i - 1]) ? 0 : 1;
+
+                    // Step 6
+                    d[i, j] = Math.Min(
+                        Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1),
+                        d[i - 1, j - 1] + cost);
+                }
+            }
+            // Step 7
+            return d[n, m];
         }
         private System.Collections.Generic.Dictionary<String, Cards.Card> cards = new System.Collections.Generic.Dictionary<String, Cards.Card>();
         private void RefreshList()
